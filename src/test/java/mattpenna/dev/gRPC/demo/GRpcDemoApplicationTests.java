76e626a596894283660cfefd59dcb6f98b4158ed
mwpenna <penna.matt@yahoo.com>
2020-06-17 13:43:09 -0400

    Initial repo creation

---
+++ src/test/java/mattpenna/dev/gRPC/demo/GRpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mattpenna.dev.gRPC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
